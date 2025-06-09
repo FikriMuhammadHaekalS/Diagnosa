591662fb51e942894b1f52358c64a1c2d5029733
FikriMuhammadHaekalS <fikrimuhammadhaekal963@gmail.com>
2025-06-09 20:32:22 +0700

    Delete Diagnosa directory

--- Diagnosa/app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('BASEURL', 'http://localhost/Diagnosa');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'diagnosa');
-define('BOOSTRAP_CSS', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
-define('BOOSTRAP_JS', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js');
-define('POPPER_JS', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
--- Diagnosa/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(!session_start()){
-  session_start();
-}
-require_once "app/init.php";
-$app = new App();
-?>
